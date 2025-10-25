bb9fe2dddd4f1b6df829604ffbc04cf8b2307ab5
mts-lucas <lmateus1067@outlook.com>
2025-10-25 18:30:21 -0300

    chore: add default rest framework settings

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -79,6 +79,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'marketplace.wsgi.application'
 
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 

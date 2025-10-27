b35dc81a93cdbf7a3b52693b1ee0a641f9807f5c
mts-lucas <lmateus1067@outlook.com>
2025-10-27 10:51:40 -0300

    chore: add test configs

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -206,5 +206,15 @@ LOGGING = {
     },
 }
 
+import sys
+
+if 'test' in sys.argv:
+    LOGGING['loggers']['django.request'] = {
+        'handlers': ['console'],
+        'level': 'ERROR',
+        'propagate': False,
+    }
+
+
 CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL', 'redis://redis:6379/0')
 CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND', 'redis://redis:6379/0')
